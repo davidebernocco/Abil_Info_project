df3e4eaec1963d560283531c83133ce9b67f4ab6
Davide Bernocco <davide.plantageneto@gmail.com>
2023-03-06 12:28:39 +0100

    Update Bash_Script.sh

--- Bash_Script.sh
+++ Bash_Script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Hello dear user, if you still haven't done it yet, PLEASE SPECIFY THE PATH IN THE BASH FILE"
+echo "Hello dear user, if you still haven't done it yet, PLEASE SPECIFY THE PATH ON THE BASH FILE"
 
 # SPECIFY THE LOCATION WHERE YOU WOULD LIKE THE FOLDER TO BE CREATED:
 path=/choose/the/location/you/prefer/on/your/device
@@ -10,6 +10,10 @@ mkdir $path/Bernocco_AbilInfo
 
 # Download the 'perAbInf.tgz' folder from the given link
 wget -P $path/Bernocco_AbilInfo/ https://adlibitum.oats.inaf.it/monaco/etc/perAbInf.tgz
+
+# Download the jupyter notebook alongside the Statistical_functions library
+wget -P $path/Bernocco_AbilInfo/ https://github.com/davidebernocco/Abil_Info_project/blob/main/AbilInfo.ipynb
+wget -P $path/Bernocco_AbilInfo/ https://github.com/davidebernocco/Abil_Info_project/blob/main/Statistical_functions.py
 
 # Extract the compressed file in the same folder and delete the compressed one
 tar -xzf $path/Bernocco_AbilInfo/perAbInf.tgz -C $path/Bernocco_AbilInfo/
